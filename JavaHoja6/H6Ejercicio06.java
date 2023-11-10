cd6f6caf5527731308e977a28364e281058d34c7
Dani47 <87781904+Danii47@users.noreply.github.com>
2023-11-10 18:34:24 +0100

    feat: h6 exercise 6

--- JavaHoja6/H6Ejercicio06.java
+++ JavaHoja6/H6Ejercicio06.java
@@ -1,61 +1,37 @@
-// CHARAT
-// String.charAt(int k) -> Te devuelve la letra que está en esa posición.
-// EJ: "Hola".charAt(2) -> "l"
-// EJ: "Hola".charAt(0) -> "H"
-
-// INDEXOF
-// String.indexOf(String stringToSearch, int firstIndexToSearch (optional)) -> Te devuelve la posición donde encuentra el primer stringToSearch.
-// EJ: "Hola mundo".indexOf("mundo") -> 5 (int)
-// EJ: "mundo Hola mundo".indexOf("mundo") -> 0 (int)
-// EJ: "mundo Hola mundo".indexOf("mundo", 1) -> 11 (int)
-
+/*
+  Escriba un método que devuelva el valor numérico entero de una cadena,
+  imponiendo como precondición que la cadena argumento contenga solamente
+  dígitos.
+*/
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class H6Ejercicio06 {
   public static void main(String[] args) {
+
     Scanner in = new Scanner(System.in);
 
-    System.out.println("Introduce una cadena para comprobar si cumple las condiciones:");
+    System.out.println("Introduce una cadena de caracteres:");
     String str = in.nextLine();
+
     in.close();
 
-    boolean passConditions = comprobateConditions(str);
+    int num = stringToInt(str);
 
-    System.out.println("La cadena " + ((passConditions) ? "sí" : "no") + " cumple las condiciones.");
+    System.out.println("El valor numérico de la cadena '" + str + "' es " + num + ".");
 
   }
 
-  public static boolean comprobateConditions(String str) {
-    return (comprobateFirstCondition(str) && comprobateSecondCondition(str) && comprobateThirdCondition(str));
-  }
+  public static int stringToInt(String str) {
 
-  public static boolean comprobateFirstCondition(String str) {
-    return (
-      (str.charAt(0) <= '9' && str.charAt(0) >= '0') &&
-      (str.charAt(1) <= '9' && str.charAt(1) >= '0')
-    );
-  }
+    int num = 0;
 
-
-  public static boolean comprobateSecondCondition(String str) {
-    return (
-      str.charAt(2) == 'N' ||
-      str.charAt(2) == 'S' ||
-      str.charAt(2) == 'E' ||
-      str.charAt(2) == 'O'
-    );
-  }
-
-  public static boolean comprobateThirdCondition(String str) {
-    boolean passCondition = false;
-    if (str.length() == 4) {
-      passCondition = (str.charAt(3) <= '9' && str.charAt(3) >= '0');
-    } else if (str.length() == 5) {
-      passCondition = (str.charAt(3) == '1' && str.charAt(4) == '0');
-    } else {
-      System.out.println("El número debe tener como máximo longitud 5");
+    for (int i = str.length() - 1; i >= 0; i--) {
+      num += (Character.getNumericValue(str.charAt(i)) * Math.pow(10, str.length() - 1 - i));
     }
-    return passCondition;
+
+    return num;
+
   }
 }
