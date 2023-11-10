889b08feb670aa606fa76d3767d30808ac38168b
Dani47 <87781904+Danii47@users.noreply.github.com>
2023-11-10 18:34:31 +0100

    feat: h6 exercise 7

---
+++ JavaHoja6/H6Ejercicio07.java
@@ -0,0 +1,69 @@
+// ! CHARAT
+// String.charAt(int k) -> Te devuelve la letra que está en esa posición.
+// EJ: "Hola".charAt(2) -> "l"
+// EJ: "Hola".charAt(0) -> "H"
+
+// ! INDEXOF
+// String.indexOf(String stringToSearch, int firstIndexToSearch (optional)) -> Te devuelve la posición donde encuentra el primer stringToSearch.
+// EJ: "Hola mundo".indexOf("mundo") -> 5 (int)
+// EJ: "mundo Hola mundo".indexOf("mundo") -> 0 (int)
+// EJ: "mundo Hola mundo".indexOf("mundo", 1) -> 11 (int)
+
+/*
+  Escriba una función que determine si una cadena de caracteres cumple o no
+  todas las condiciones siguientes:
+  * a) los dos primeros caracteres son dígitos
+  * b) el tercer carácter es una de las letras N, S, E ó O
+  * c) a continuación hay 1 ó dos dígitos, cuyo valor numérico es un entero
+  *    entre 1 y 10
+*/
+
+import java.util.Scanner;
+
+public class H6Ejercicio07 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Introduce una cadena para comprobar si cumple las condiciones:");
+    String str = in.nextLine();
+    in.close();
+
+    boolean passConditions = comprobateConditions(str);
+
+    System.out.println("La cadena " + ((passConditions) ? "sí" : "no") + " cumple las condiciones.");
+
+  }
+
+  public static boolean comprobateConditions(String str) {
+    return (comprobateFirstCondition(str) && comprobateSecondCondition(str) && comprobateThirdCondition(str));
+  }
+
+  public static boolean comprobateFirstCondition(String str) {
+    return (
+      (str.charAt(0) <= '9' && str.charAt(0) >= '0') &&
+      (str.charAt(1) <= '9' && str.charAt(1) >= '0')
+    );
+  }
+
+
+  public static boolean comprobateSecondCondition(String str) {
+    return (
+      str.charAt(2) == 'N' ||
+      str.charAt(2) == 'S' ||
+      str.charAt(2) == 'E' ||
+      str.charAt(2) == 'O'
+    );
+  }
+
+  public static boolean comprobateThirdCondition(String str) {
+    boolean passCondition = false;
+    if (str.length() == 4) {
+      passCondition = (str.charAt(3) <= '9' && str.charAt(3) >= '0');
+    } else if (str.length() == 5) {
+      passCondition = (str.charAt(3) == '1' && str.charAt(4) == '0');
+    } else {
+      System.out.println("El número debe tener como máximo longitud 5");
+    }
+    return passCondition;
+  }
+}
