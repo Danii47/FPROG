976212f9081889d7e90871f53109365faad64f56
Dani47 <87781904+Danii47@users.noreply.github.com>
2023-11-16 22:52:38 +0100

    Add code for EAN validation and vector operations

---
+++ Examen/ExamenPapel.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class ExamenPapel {
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.print("Introduce el código EAN: ");
+
+    String codigo = in.nextLine();
+    in.close();
+
+    if (EANValido(codigo))
+      System.out.println("El código es válido.");
+    else
+      System.out.println("El código no es válido.");
+
+  }
+
+  public static boolean sonNumeros(String cadena) {
+
+    boolean esNumero = true;
+
+    for (int i = 0; i < cadena.length() && esNumero; i++) {
+      if (cadena.charAt(i) < '0' || cadena.charAt(i) > '9')
+        esNumero = false;
+    }
+    return esNumero;
+
+  }
+
+  public static boolean EANValido(String codigo) {
+
+    boolean esValido = true;
+
+    if (codigo.length() != 13)
+      esValido = false;
+    if (!sonNumeros(codigo))
+      esValido = false;
+
+    int suma = 0;
+
+    for (int i = 11; i >= 0 && esValido; i--) {
+      int digito = codigo.charAt(i) - '0';
+      if (i % 2 == 0)
+        suma += digito;
+      else
+        suma += (digito * 3);
+    }
+
+    if (esValido) {
+      if (suma % 10 == 0) {
+        if (codigo.charAt(12) != '0')
+          esValido = false;
+      } else {
+        int digito = 10 - (suma % 10);
+        if (codigo.charAt(12) - '0' != digito)
+          esValido = false;
+      }
+    }
+
+    return esValido;
+  }
+}
---
+++ JavaHoja7/H7Ejercicio01.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class H7Ejercicio01 {
+  
+  public static void main(String[] args) {
+    
+    Scanner in = new Scanner(System.in);
+    System.out.println("Introduce la longitud del vector:");
+    int longitud = in.nextInt();
+    double[] vector = new double[longitud];
+  
+    for (int i = 0; i < vector.length; i++) {
+      System.out.println("Introduce el valor de la posición " + (i + 1) + ":");
+      vector[i] = in.nextDouble();
+    }
+
+    in.close();
+
+    System.out.println("El módulo del vector es: " + getModule(vector));
+    System.out.print("El vector unitario es: ");
+    for (int i = 0; i < vector.length; i++) {
+      System.out.print(getUnitVector(vector)[i] + " ");
+    }
+
+  }
+
+  public static double getModule(double[] vector) {
+
+    double module = 0;
+
+    for (int i = 0; i < vector.length; i++) {
+      module += Math.pow(vector[i], 2);
+    }
+
+    return Math.sqrt(module);
+  }
+
+  public static double[] getUnitVector(double[] vector) {
+
+    double[] unitVector = new double[vector.length];
+    double module = getModule(vector);
+
+    for (int i = 0; i < vector.length; i++) {
+      unitVector[i] = vector[i] / module;
+    }
+
+    return unitVector;
+  }
+}
