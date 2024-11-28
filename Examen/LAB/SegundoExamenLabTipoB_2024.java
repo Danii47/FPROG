d615d69d77a790b9229c5ae13c2467f34f87c1d7
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-11-28 15:39:13 +0100

    style comments

--- Examen/LAB/SegundoExamenLabTipoB_2024.java
+++ Examen/LAB/SegundoExamenLabTipoB_2024.java
@@ -1,5 +1,19 @@
 package LAB;
 
+/*
+Escribe un programa que simule el juego del bingo con 3 jugadores y unos cartones predefinidos. Para ello, tendremos una matriz, y cada fila representará el cartón de cada jugador. El juego tendrá 3 jugadores, y cada uno de ellos jugará con los siguientes cartones:
+Jugador1 -> 1 10 20 30 40 50 60 70 80 90
+Jugador2 -> 1 2 3 4 5 6 7 8 9 10
+Jugador3 -> 4 8 15 16 23 42 53 64 75 86
+
+El programa sacará nuevos números de forma aleatoria (que podrían repetirse). Si un número coincide con el de un cartón, este se reemplazará por el valor -1 en la matriz. Cuando un cartón tenga todos sus valores a -1 significará que ha ganado, y se mostrará un mensaje por pantalla con el número del ganador. Entonces, el juego terminará.
+
+Ejemplo:
+Inicio del juego del bingo con 3 jugadores y 10 números por cartón!
+(Números que van saliendo)
+Ganador:Jugador#3
+*/
+
 public class SegundoExamenLabTipoB_2024 {
   public static void main(String[] args) {
     int[][] cartones = {
