71ecf13baf86310f9ee683307ba3b9913d4298e1
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-01-03 23:12:32 +0100

    ejercicio 3 extraordinaria 2024

---
+++ Examen/Extraordinaria/2024/GrupoT1/Ejercicio3.java
@@ -0,0 +1,25 @@
+// Contrapalindroma
+
+public class Ejercicio3 {
+  public static boolean contrapalindroma(String cadena) {
+    // PRE: cadena != null
+    if (cadena.length() <= 1) {
+      return true; // Caso base
+    } else {
+      return (
+        cadena.charAt(0) != cadena.charAt(cadena.length() - 1) &&
+        contrapalindroma(cadena.substring(1, cadena.length() - 1))
+      ); // Caso recursivo
+    }
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(contrapalindroma("hola")); // false
+    System.out.println(contrapalindroma("abba")); // false
+    System.out.println(contrapalindroma("abca")); // false
+    System.out.println(contrapalindroma("abcba")); // false
+    System.out.println(contrapalindroma("abcde")); // true
+    System.out.println(contrapalindroma("a")); // true
+    System.out.println(contrapalindroma("")); // true
+  }
+}
